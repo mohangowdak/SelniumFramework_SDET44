6aec8de4e9a31d3dd89a645ee7601007eb22d5da
MOHAN GOWDA <103551008+mohangowdak@users.noreply.github.com>
2022-12-19 11:39:43 +0530

    Update SimpleScript.java

--- src/test/java/com/sdet44/practice/SimpleScript.java
+++ src/test/java/com/sdet44/practice/SimpleScript.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+//
 public class SimpleScript {
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
