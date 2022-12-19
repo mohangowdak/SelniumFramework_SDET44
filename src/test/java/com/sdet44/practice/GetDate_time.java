53cacba1fa085ce8b83bbe0170b45c07df5f66c3
MOHAN GOWDA <103551008+mohangowdak@users.noreply.github.com>
2022-12-19 11:39:29 +0530

    Update GetDate_time.java

--- src/test/java/com/sdet44/practice/GetDate_time.java
+++ src/test/java/com/sdet44/practice/GetDate_time.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class GetDate_time {
-	
+	//
 public static void main(String[] args) {
 	Date date=new Date();
 	SimpleDateFormat sdf=new SimpleDateFormat("dd_MM_YYYY_HH_mm_sss");
